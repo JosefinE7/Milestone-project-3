4d043d5cec14b75489f2f31ce17016a33047884d
JosefinE7 <fin.jos@hotmail.com>
2020-11-02 10:48:35 +0000

    Added Emailjs function to form in contact_me.html

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -3,10 +3,11 @@ function sendMail(contactForm) {
 		from_name: contactForm.name.value,
 		from_email: contactForm.emailaddress.value,
 		project_request: contactForm.projectsummary.value,
-	});
-	then(
+	})
+	.then(
 		function (response) {
 			console.log("SUCCESS", response);
+            document.getElementById("form-rest").reset();
 		},
 		function (error) {
 			console.log("FAILED", error);
