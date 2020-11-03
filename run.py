1685b76f8e120eeada3f9f474cda25a42f9c469f
JosefinE7 <fin.jos@hotmail.com>
2020-11-03 11:26:18 +0000

    Bug fix in contact form in contact_me.html

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,17 +1,18 @@
 function sendMail(contactForm) {
-	emailjs.send("gmail", "erica", {
-		from_name: contactForm.name.value,
-		from_email: contactForm.emailaddress.value,
-		project_request: contactForm.projectsummary.value,
-	})
-	.then(
-		function (response) {
-			console.log("SUCCESS", response);
-            document.getElementById("form-rest").reset();
-		},
-		function (error) {
-			console.log("FAILED", error);
-		}
-	);
+	emailjs
+		.send("gmail", "erica", {
+			from_name: contactForm.name.value,
+			from_email: contactForm.emailaddress.value,
+			project_request: contactForm.projectsummary.value,
+		})
+		.then(
+			function (response) {
+				console.log("SUCCESS", response);
+				document.getElementById("form-rest").reset();
+			},
+			function (error) {
+				console.log("FAILED", error);
+			}
+		);
 	return false;
 }
